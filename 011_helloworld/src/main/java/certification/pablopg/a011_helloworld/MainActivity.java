8898df9404d1f458d8ef2d90d1403bc5fa63601a
Pablo <pablopg@us.es>
2019-01-01 14:21:55 +0100

    Android Codelab 1 Completed.

--- 011_helloworld/src/main/java/certification/pablopg/a011_helloworld/MainActivity.java
+++ 011_helloworld/src/main/java/certification/pablopg/a011_helloworld/MainActivity.java
@@ -2,12 +2,16 @@ package certification.pablopg.a011_helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
     }
 }
